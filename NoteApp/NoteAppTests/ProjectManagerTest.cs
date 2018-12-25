8f6d7705bec772696b8e2689dda2d800f7bb1cc1
HOME <HOME@DNS>
2018-12-25 20:26:47 +0700

    Написаны тесты для сериализации и десериализации

--- NoteApp/NoteAppTests/ProjectManagerTest.cs
+++ NoteApp/NoteAppTests/ProjectManagerTest.cs
@@ -7,6 +7,23 @@ namespace NoteAppTests
     [TestFixture]
     public class ProjectManagerTest
     {
-        
+        [Test(Description = "")]
+        public void TestProjectManagerSaveToFile()
+        {
+            Note note = new Note();
+            note.Text = "Смирнов";
+            var project = new Project();
+            project.Notes.Add(note);
+            ProjectManager.Save(project, "c:\\Users\\User\\Desktop\\111.json");
+
+            var actual = ProjectManager.Load("c:\\Users\\User\\Desktop\\111.json");
+
+            var expected = project;
+
+            Assert.AreEqual(expected.Notes.Count, actual.Notes.Count, "Different count of notes in project");
+
+            Assert.AreEqual(expected.Notes[0].Text, actual.Notes[0].Text, "Сериализация работает неверно!");
+
+        }
     }
 }
